7512b0db8bbc51c3e3db077af2e1cf27c3972efc
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2022-11-03 00:42:56 +0530

    Create iterator.cpp

---
+++ iterator.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int>var1{1,3,4,5,6,7};
+    vector<int>::iterator iter;
+    iter = var1.end()-1;
+    cout<<*iter<<" ";
+    return 0;
+}
