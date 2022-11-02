79da0df74cf201fbefa18adf8af5c70051cad2f6
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2022-11-03 00:11:40 +0530

    Create change vector elements.cpp

---
+++ change vector elements.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int>var1{1,3,4,5,6,7};
+    var1.at(1) = 2;
+    var1.at(2) = 9;
+    for(const int& i : var1)
+    {
+        cout<<i<<" ";
+    }
+    return 0;
+}
