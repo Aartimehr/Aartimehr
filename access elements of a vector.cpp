bcb2beb8e225d69e5d9bf36ade65807b935d673e
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2022-11-02 23:59:19 +0530

    Create access elements of a vector.cpp

---
+++ access elements of a vector.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int>var1{1,3,4,5,6,7};
+    cout<<var1.at(0)<<" ";
+    cout<<var1.at(1)<<" ";
+    cout<<var1.at(5)<<" ";
+    cout<<var1.at(3)<<" ";
+    return 0;
+    
+}
