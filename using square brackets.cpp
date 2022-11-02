f34e50e50f324e3466855994863787fce90428e4
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2022-11-03 00:02:05 +0530

    Create using square brackets.cpp

---
+++ using square brackets.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int>var1{1,3,4,5,6,7};
+    cout<<var1[0]<<" ";
+    cout<<var1[1]<<" ";
+    cout<<var1[5]<<" ";
+    cout<<var1[3]<<" ";
+    return 0;
+    
+}
