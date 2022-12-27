87c7cfc8faa30b4985565d4b7c3eb9e2b64b0906
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2022-12-27 22:12:16 +0530

    Create Number system.cpp

---
+++ Number system.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int>var1{1,3,4,5,6,7};
+     var1.push_back(8);
+     var1.push_back(9);
+     for(const int& i : var1)
+     {
+         cout<<i<<" ";
+     }
+    return 0;
+    
+}
+Footer
