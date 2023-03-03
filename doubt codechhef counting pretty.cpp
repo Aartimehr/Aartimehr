8f14ea56fd44d83be49ede4b4e478d65bfb7c32e
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2023-03-03 23:06:07 +0530

    Create doubt codechhef counting pretty.cpp

---
+++ doubt codechhef counting pretty.cpp
@@ -0,0 +1,15 @@
+include <iostream>
+using namespace std;
+
+int main() {
+   int T;
+   cin>>T;
+   vector<int>ans;
+   while(T--)
+   {
+       int L,R;
+       cin>>L>>R;
+       
+   }
+	return 0;
+}
